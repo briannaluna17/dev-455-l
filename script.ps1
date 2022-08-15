9f1fd1f9cbb66f2c2b67934a77bfd90328c28f39
briannaluna17 <briannaluna3@yahoo.com>
2022-08-15 20:04:03 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -3,3 +3,4 @@ Temporary commit content for 2022-06-27T20:04:03
 Temporary commit content for 2022-07-09T20:04:03
 Temporary commit content for 2022-07-23T20:04:03
 Temporary commit content for 2022-07-31T20:04:03
+Temporary commit content for 2022-08-15T20:04:03
