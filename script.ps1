a756bdd5de84fb91e942324ec0756a53a50d5a7b
briannaluna17 <briannaluna3@yahoo.com>
2024-04-22 20:04:03 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -55,3 +55,4 @@ Temporary commit content for 2024-03-27T20:04:03
 Temporary commit content for 2024-04-08T20:04:03
 Temporary commit content for 2024-04-10T20:04:03
 Temporary commit content for 2024-04-20T20:04:03
+Temporary commit content for 2024-04-22T20:04:03
