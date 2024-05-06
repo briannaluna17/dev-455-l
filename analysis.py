02318dafc747572f74bdebdbb513e1b65ed1e565
briannaluna17 <briannaluna3@yahoo.com>
2024-05-06 20:04:03 +0100

    Enhanced UI responsiveness

--- analysis.py
+++ analysis.py
@@ -61,3 +61,4 @@ Temporary commit content for 2024-04-06T20:04:03
 Temporary commit content for 2024-04-09T20:04:03
 Temporary commit content for 2024-04-26T20:04:03
 Temporary commit content for 2024-05-01T20:04:03
+Temporary commit content for 2024-05-06T20:04:03
