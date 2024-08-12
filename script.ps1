decc0ac4cdd552fa435b4a44c4a3ec95c2edbbd4
briannaluna17 <briannaluna3@yahoo.com>
2024-08-12 20:04:03 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -65,3 +65,4 @@ Temporary commit content for 2024-06-27T20:04:03
 Temporary commit content for 2024-07-14T20:04:03
 Temporary commit content for 2024-07-21T20:04:03
 Temporary commit content for 2024-08-07T20:04:03
+Temporary commit content for 2024-08-12T20:04:03
