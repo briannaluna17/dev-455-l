fccba9d7c07aed853a7cf2aa7ef9e8da06df8957
briannaluna17 <briannaluna3@yahoo.com>
2024-11-15 20:04:03 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -70,3 +70,4 @@ Temporary commit content for 2024-09-16T20:04:03
 Temporary commit content for 2024-10-06T20:04:03
 Temporary commit content for 2024-10-13T20:04:03
 Temporary commit content for 2024-10-28T20:04:03
+Temporary commit content for 2024-11-15T20:04:03
